5464a209b28260d009717e5e44c47fb52579a7a3
Christopher Kath <chris.kath@web.de>
2018-11-10 15:20:56 +0100

    neu

--- Data/Data_Preparation.py
+++ Data/Data_Preparation.py
@@ -8,13 +8,12 @@ os.chdir('/Users/Chris/Documents/GitHub/EUA-Price-Forecaster/Data')
 
 "CSV file reading"
 import pandas as pd 
-data = pd.read_csv("input.csv",sep=';') 
+data = pd.read_csv("input.csv",sep=';',parse_dates=['Date']) 
 
 "missing data"
-import numpy
-# mark zero values as missing or NaN
-data.iloc[2:26] = data.iloc[2,:26].replace(0, numpy.NaN)
-# fill missing values with mean column values
-data.iloc[2:26].fillna(data.iloc[2:26].mean(), inplace=True)
-# count the number of NaN values in each column
+import numpy as np
+data.loc[:, data.columns[1:]] = data.loc[:, data.columns[1:]].replace(0, np.NaN)
+data.loc[:, data.columns[1:]] = data.loc[:, data.columns[1:]].replace(np.NaN, data.loc[:, data.columns[1:]].mean())
 print(data.isnull().sum())
+
+data.info()
